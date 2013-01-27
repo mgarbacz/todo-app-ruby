849ea6ff43e8a52a4d85a41a16966abf6078df28
mgarbacz <mich.garbacz@gmail.com>
2013-01-26 22:25:22 -0600

    Added create todo via POST functionality

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,17 @@
 class TodosController < ApplicationController
+
+  # Declaring formats for all views
+  respond_to :html, :json
+
   def index 
     @todos = Todo.all
+
+    respond_with(@todos)
+  end
+
+  def create
+    @todo = Todo.create(params[:todo])
+
+    respond_with(@todo, :location => root_path)
   end
 end
--- spec/controllers/todo_controller_spec.rb
+++ spec/controllers/todo_controller_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
-describe TodoController do
+describe TodosController do
 
-  describe "GET 'todos'" do
-    it "returns http success" do
-      get 'todos'
+  def valid_attributes_hash
+    { :todo => 'Test todo', :done => true }
+  end
+
+  describe 'GET "todos"' do
+    it 'returns http success' do
+      get 'index'
       response.should be_success
+    end
+  end
+
+  describe 'POST create' do
+    describe 'with valid params' do
+      it 'creates a new Todo' do
+        expect {
+          post :create, { :todo => valid_attributes_hash }, {}
+        }.to change(Todo, :count).by(1)
+      end
     end
   end
 
