a011b0cc9f79059abb63f80d3a3608d852df7821
mgarbacz <mich.garbacz@gmail.com>
2013-01-27 18:47:49 -0600

    Added tests for ajax functionality, I strayed from TDD with that!

--- spec/controllers/todo_controller_spec.rb
+++ spec/controllers/todo_controller_spec.rb
@@ -20,6 +20,16 @@ describe TodosController do
           post :create, { :todo => valid_attributes_hash }, {}
         }.to change(Todo, :count).by(1)
       end
+
+      it 'assigns a newly created todo as todo' do
+        post :create, { :todo => valid_attributes_hash }, {}
+        assigns(:todo).should be_a(Todo)
+      end
+
+      it 'responds with the partial for created todo' do
+        post :create, { :todo => valid_attributes_hash }, {}
+        response.should render_template('todos/_todo')
+      end
     end
   end
 
